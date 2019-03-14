548df1bea7f8cf1890d96fe074853ffb06456ec7
RafaMarquesLearn <30332657+RafaMarquesLearn@users.noreply.github.com>
2019-03-14 10:10:00 -0300

    Add first scripts and Jenkinsfile

---
+++ build.ps1
@@ -0,0 +1,13 @@
+# Path to Msbuild tool
+
+$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
+set-alias msbuild $msbuild
+
+# solution settings
+$sln_name = "C:\Users\rafael_marques\Documents\code\Projeto2\windows\Projeto2.sln"
+$vs_config = "DebugBundle" 
+$vs_platfom = "x64"
+
+# call the build method
+Write-Host "Building solution`n" -foregroundcolor Green
+msbuild $sln_name /t:Build /p:Configuration=$vs_config /p:Platform=$vs_platfom /v:q /nologo
