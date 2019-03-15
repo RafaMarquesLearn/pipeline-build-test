5eea4ee51107e6a537b96936132930f547c805f1
Rafael Marques <rafa.marques.learn@gmail.com>
2019-03-15 18:13:26 -0300

    created branch for game app and altered files accordingly

--- prepare.ps1
+++ prepare.ps1
@@ -1,7 +1,7 @@
 Set-ExecutionPolicy Bypass -Scope Process
 
-& 'C:\Users\rafael_marques\Downloads\nuget.exe' restore "C:\Users\rafael_marques\Documents\code\Projeto2\windows\Projeto2.sln" -ConfigFile "C:\Users\rafael_marques\AppData\Roaming\NuGet\NuGet.Config" -NoCache
+& 'C:\Users\rafael_marques\Downloads\nuget.exe' restore "C:\Users\rafael_marques\Desktop\MinefieldWindows\windows\MinefieldWindows.sln" -ConfigFile "C:\Users\rafael_marques\AppData\Roaming\NuGet\NuGet.Config" -NoCache
 
-cd C:\Users\rafael_marques\Documents\code\Projeto2
+cd C:\Users\rafael_marques\Desktop\MinefieldWindows
 
-& 'react-native' bundle --platform windows --entry-file index.js --bundle-output "C:\Users\rafael_marques\Documents\code\Projeto2\windows\Projeto2\ReactAssets\index.windows.bundle" --assets-dest "C:\Users\rafael_marques\Documents\code\Projeto2\windows\Projeto2\ReactAssets" --dev false
+& 'react-native' bundle --platform windows --entry-file index.js --bundle-output "C:\Users\rafael_marques\Desktop\MinefieldWindows\windows\MinefieldWindows\ReactAssets\index.windows.bundle" --assets-dest "C:\Users\rafael_marques\Desktop\MinefieldWindows\windows\MinefieldWindows\ReactAssets" --dev false
