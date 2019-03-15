e2cb5e9e8bbf9cf1c09f6a8a06957456d80dd171
RafaMarquesLearn <30332657+RafaMarquesLearn@users.noreply.github.com>
2019-03-15 10:50:29 -0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -6,9 +6,11 @@ set-alias msbuild $msbuild
 # solution settings
 #$sln_name = "C:\Users\rafael_marques\Documents\code\Projeto2\windows\Projeto2.sln"
 $sln_name = C:\Users\rafael_marques\Desktop\MinefieldWindows\windows\MinefieldWindows.sln
-$vs_config = "DebugBundle" 
+#$vs_config = "DebugBundle"
+$vs_config = "Release"
 $vs_platfom = "x64"
 
 # call the build method
 Write-Host "Building solution`n" -foregroundcolor Green
-msbuild $sln_name /t:Build /p:Configuration=$vs_config /p:Platform=$vs_platfom /v:q /nologo
+#msbuild $sln_name /t:Build /p:Configuration=$vs_config /p:Platform=$vs_platfom /v:q /nologo
+msbuild $sln_name /t:Build /p:Configuration=$vs_config /p:Platform=$vs_platfom /p:PackageAsSingleFile=true /v:q /nologo
