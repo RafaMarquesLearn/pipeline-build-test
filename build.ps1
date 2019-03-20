3289a79c418e9939eff14acb1d2a0789e4494bd3
Rafael Marques <rafa.marques.learn@gmail.com>
2019-03-20 14:49:27 -0300

    Changed code to prepare and build app, working locally

--- build.ps1
+++ build.ps1
@@ -1,16 +1,13 @@
 # Path to Msbuild tool
+$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
-$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe"
 set-alias msbuild $msbuild
 
 # solution settings
-#$sln_name = "C:\Users\rafael_marques\Documents\code\Projeto2\windows\Projeto2.sln"
 $sln_name = "C:\Users\rafael_marques\Documents\code\react-native\win-app-shell\HPAppShell\windows\HPAppShell.sln"
-#$vs_config = "DebugBundle"
-$vs_config = "Release"
+$vs_config = "DebugBundle"
 $vs_platfom = "x64"
 
 # call the build method
 Write-Host "Building solution`n" -foregroundcolor Green
-#msbuild $sln_name /t:Build /p:Configuration=$vs_config /p:Platform=$vs_platfom /v:q /nologo
-msbuild $sln_name /t:rebuild /p:Configuration=$vs_config /p:Platform=$vs_platfom /v:q /nologo
+msbuild $sln_name /t:Build /p:Configuration=$vs_config /p:Platform=$vs_platfom /v:q /nologo
